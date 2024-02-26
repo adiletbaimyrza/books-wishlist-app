6a41932d89dbb042fae5e553317594597795e50f
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-02-26 18:35:57 +0100

    Update book link to include app name in URL

--- src/components/GridBook.tsx
+++ src/components/GridBook.tsx
@@ -32,7 +32,7 @@ const GridBook = ({
     : 'No ratings'
 
   return (
-    <Link id={`id-${id}`} className="sb-card" to={`/${id}`}>
+    <Link id={`id-${id}`} className="sb-card" to={`/books-wishlist-app/${id}`}>
       <div className="sb-thumbnail-wrapper">
         {smallThumbnail ? (
           <img src={smallThumbnail} className="sb-thumbnail" />

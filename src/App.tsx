e5186f94b8de083bb3f7227c8ae13eb7fba6dcf7
Adilet Baimyrza <adiletbaimyrza@gmail.com>
2024-02-28 12:25:05 +0100

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,7 @@
-import {
-  FavouritesPage,
-  HomePage,
-  ReadPage,
-  ToReadPage,
-  NotFoundPage,
-  SearchPage,
-  SingleBookPage,
-  ReviewsPage,
-} from './pages'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import RecommendedPage from './pages/RecommendedPage'
+import routes from './utils/routes'
 
-const router = createBrowserRouter([
-  {
-    path: '/books-wishlist-app/home',
-    element: <HomePage />,
-    errorElement: <NotFoundPage />,
-  },
-  {
-    path: '/books-wishlist-app/favourites/',
-    element: <FavouritesPage />,
-  },
-  {
-    path: '/books-wishlist-app/toread/',
-    element: <ToReadPage />,
-  },
-  {
-    path: '/books-wishlist-app/read/',
-    element: <ReadPage />,
-  },
-  {
-    index: true,
-    path: '/books-wishlist-app/search/',
-    element: <SearchPage />,
-  },
-  {
-    path: '/books-wishlist-app/:id/',
-    element: <SingleBookPage />,
-  },
-  {
-    path: '/books-wishlist-app/recommended/',
-    element: <RecommendedPage />,
-  },
-  {
-    path: '/books-wishlist-app/reviews/',
-    element: <ReviewsPage />,
-  },
-])
+const router = createBrowserRouter(routes)
 
 function App() {
   return <RouterProvider router={router} />
---
+++ src/utils/utils.types.ts
@@ -0,0 +1,8 @@
+type Route = {
+  index?: boolean
+  path: string
+  element: React.ReactNode
+  errorElement?: React.ReactNode
+}
+
+export type { Route }
